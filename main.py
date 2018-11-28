ba9ea8c4a8be05708210ec513495d551418d12ec
BuildTools <unconfigured@null.spigotmc.org>
2018-11-27 23:05:36 -0600

    Loaded from home desktop. Test commit.

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import sys
 import pygame
-pygame.init()
-print("Hello Snabe")
+from settings import Settings
+
+def run_game():
+    pygame.init()
+
+    snabings = Settings()
+    screen = pygame.display.set_mode((snabings.screen_width, snabings.screen_height))
+    pygame.display.set_caption("Snabe")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(snabings.background_color)
+
+        pygame.display.flip()
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Snabe settings and constants"""
+
+    def __init__(self):
+        """Init the game settings"""
+        # screen settings
+        self.screen_width = 800
+        self.screen_height = 800
+        self.background_color = (230, 230, 230)
